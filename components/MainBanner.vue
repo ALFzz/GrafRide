9690c186d7251fecaedb79e1cddf2549102e6762
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-07-05 22:10:18 +0300

    add new font and new color

--- components/MainBanner.vue
+++ components/MainBanner.vue
@@ -11,7 +11,7 @@
         <span class="block h-0.5 ml-28 w-52 bg-gradient-to-r from-white via-white to-transparent"></span>
         <span class="block h-0.5 ml-44 w-52 bg-gradient-to-r from-white via-white to-transparent"></span>
       </div>
-      <h1 class="font-gothic mt-10 text-[70px] leading-none text-transparent text-stroke-2 text-stroke-color-white">Прокачай <br> свою тачку</h1>
+      <h1 class="font-gothic mt-10 text-[70px] leading-none text-transparent text-stroke-1 text-stroke-color-white">Прокачай <br> свою тачку</h1>
       <img class=" mt-5 self-end w-[400px]" src="public/images/pump.png" alt="pump">
       <NuxtLink class="self-end" to="/services">
         <button class="p-3 mt-10 px-10  w-fit font-ruberoid font-extrabold  text-center bg-neonpink text-black rounded-full">Перейти к услугам</button>
@@ -23,9 +23,14 @@
           alt="supra"
           class="w-full h-full object-cover opacity-60"
       />
-      <div class="absolute inset-0 bg-gradient-to-r from-[#171318] via-transparent to-black/50"></div>
+      <div class="absolute inset-0 bg-gradient-to-r from-dark via-transparent to-black/50"></div>
+    </div>
+    <div class="absolute right-0 bottom-0 -mb-6 self-end">
+      <h1 class="font-kobar self-end text-[100px] text-transparent text-stroke-1 text-stroke-color-white">SAVAGE</h1>
     </div>
   </div>
+
+
 </template>
 
 <style scoped>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen text-white bg-[#171318]">
+  <div class="flex flex-col min-h-screen text-white bg-dark">
   <Header/>
     <slot></slot>
   </div>
