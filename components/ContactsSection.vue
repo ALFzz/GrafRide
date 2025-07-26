4bbd764026da7a79e99a91f45cef9db70f63f501
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-07-26 15:59:37 +0300

    add Contact item

---
+++ components/ContactsSection.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <UITitle title="contacts" sub-title="Контакты" />
+    <div class="flex flex-row">
+      <div class="">
+        <div class="grid grid-cols-2 gap-16">
+          <ContactItem
+              title="Контактный  номер"
+              sub-title="8 (499) 423-63-56"
+          />
+          <ContactItem
+              title="Адрес"
+              sub-title="Москва, Костянский пер., 4"
+          />
+          <ContactItem
+              title="Электронная почта"
+              sub-title="grafride@gmail.ru"
+          />
+          <ContactItem
+              title="Режим работы"
+              sub-title="Ежедневно с 10.00 до 21.00"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
